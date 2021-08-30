d7bb5f3b0239242dc63ced915d30d42a1314dbbf
chadelofson <chad.elofson@gmail.com>
2021-08-30 01:06:19 -0700

    Preparing for deployment

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,16 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const AboutPage = () => {
+  return (
+    <>
+      <main>
+        <p>
+          This site was created to demonstrate my mastery of Redwood: Look on my
+          works, ye mighty, and despair!
+        </p>
+      </main>
+    </>
+  )
+}
+
+export default AboutPage
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogPostsCell from 'src/components/BlogPostsCell'
+
+const HomePage = () => {
+  return (
+    <>
+      <main>Home</main>
+      <BlogPostsCell />
+    </>
+  )
+}
+
+export default HomePage
